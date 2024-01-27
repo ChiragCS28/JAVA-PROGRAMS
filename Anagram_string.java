7ba5a25962f9c37de00f020f2836c848730b1722
Chirag C S <156529214+ChiragCS28@users.noreply.github.com>
2024-01-27 20:17:41 +0530

    Anagram_string.java

--- Anagram_string.java
+++ Anagram_string.java
@@ -1,37 +1,45 @@
+// An anagram is a word or phrase formed by rearranging the letters of another word or phrase, using all the original letters exactly once //
+
 import java.util.Scanner;
 import java.util.*;
-class anagram{
-    public static void main(String[]args){
-        Scanner sc= new Scanner(System.in);
+
+class Anagram {
+    public static void main(String[] args) {
+        // Taking user input for two strings
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter String 1:");
         String s1 = sc.nextLine();
         System.out.println("Enter String 2:");
         String s2 = sc.nextLine();
-        
-        char[]a1 = s1.toCharArray();  // converting the string to an array of characters //
-        char[]a2 = s2.toCharArray();
-        
-        Arrays.sort(a1); // Sorting the arrays //
+
+        char[] a1 = s1.toCharArray();  // converting the string to an array of characters //
+        char[] a2 = s2.toCharArray();
+
+        // Sorting the character arrays
+        Arrays.sort(a1); 
         Arrays.sort(a2);
-        
-        int n =s1.length();
+
+        // Getting the length of the strings
+        int n = s1.length();
         int m = s2.length();
-        if(n!=m){
+
+        // Checking if the lengths are equal
+        if (n != m) {
             System.out.println("not an anagram");
-        }
-        else{
-            int count=0;
-            for(int i=0;i<n;i++){
-                if(a1[i]!=a2[i]){
+        } else {
+            int count = 0;
+            // Comparing each character of the sorted arrays
+            for (int i = 0; i < n; i++) {
+                if (a1[i] != a2[i]) {
                     System.out.println("not an anagram");
                     count++;
                     break;
                 }
             }
-            if(count==0){
+            // If no mismatches found, it's an anagram
+            if (count == 0) {
                 System.out.println("an anagram");
             }
         }
-        
     }
 }
